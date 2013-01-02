18c66b1a72e872ceaa0a430c18e94cca040b92c9
Kayo Phoenix <kayo@illumium.org>
2013-01-02 23:33:58 +0600

    Layout support added..

--- pcbjs.js
+++ pcbjs.js
@@ -1,14 +1,21 @@
 define(['peg!./gerber', 'peg!./excellon', './gerber.render', './excellon.render'], function(Gerber, Excellon, GerberRender, ExcellonRender){
-  function Board(){
+  function Board(layout){
+    this.box = {
+      x: [null, null],
+      y: [null, null]
+    };
+    this.layout = layout;
     this.layers = {};
   }
   function Layer(data){
+    this.enable = true;
     if(data){
       this.load(data);
     }
   }
 
-  var excellon_cookie_re = /FMAT,/;
+  var und, M = Math, min = M.min, max = M.max,
+  excellon_cookie_re = /FMAT,/;
 
   function parseError(e){
     return e.message + (e.line ? ' (' + e.line + ':' + e.column + ')' : '');
@@ -16,40 +23,92 @@ define(['peg!./gerber', 'peg!./excellon', './gerber.render', './excellon.render'
 
   Layer.prototype = {
     load: function(data){
+      var self = this;
       if(excellon_cookie_re.test(data)){
         try{
-          this.data = Excellon.parse(data);
-          this.type = 'excellon';
+          self.data = Excellon.parse(data);
+          self.type = 'excellon';
         }catch(e){
-          this.error = parseError(e);
+          self.error = parseError(e);
           return false;
         }
       }else{
         try{
-          this.data = Gerber.parse(data);
-          this.type = 'gerber';
+          self.data = Gerber.parse(data);
+          self.type = 'gerber';
         }catch(e){
-          this.error = parseError(e);
+          self.error = parseError(e);
           return false;
         }
+      }
+      self.box = self.data.box;
+      if(self.board){ /* recalc bound */
+        self.board.bound();
       }
       return true;
     },
     draw: function(ppi, color, hatch){
-      ppi = ppi || 600;
-      color = color || 'black';
-      if(this.type == 'gerber'){
+      var self = this,
+      layout = self.board && self.board.layout && self.board.layout[self.name] || {},
+      opts = {
+        box: self.board ? self.board.box : self.box,
+        ppi: ppi || 600,
+        color: color || layout.color || 'black',
+        hatch: hatch !== undefined ? hatch : layout.hatch
+      };
+      if(self.type == 'gerber'){
         if(typeof hatch != 'boolean') hatch = true;
-        return GerberRender.render(this.data, ppi, color, hatch);
-      }else if(this.type == 'excellon'){
-        return ExcellonRender.render(this.data, ppi, color);
+        return GerberRender.render(self.data, opts);
+      }else if(self.type == 'excellon'){
+        return ExcellonRender.render(self.data, opts);
       }
     }
   };
 
   Board.prototype = {
-    layer: function(name, type, layer){ /* set layer */
+    bound: function(){ /* recalc bound */
+      var name,
+      lay,
+      box = this.box,
+      layers = this.layers,
+      once = false;
 
+      for(name in layers){
+        lay = layers[name].box;
+        if(once){
+          box.x[0] = min(box.x[0], lay.x[0]);
+          box.x[1] = max(box.x[1], lay.x[1]);
+          box.y[0] = min(box.y[0], lay.y[0]);
+          box.y[1] = max(box.y[1], lay.y[1]);
+        }else{
+          once = true;
+          box.x[0] = lay.x[0];
+          box.x[1] = lay.x[1];
+          box.y[0] = lay.y[0];
+          box.y[1] = lay.y[1];
+        }
+      }
+    },
+    layer: function(name, layer){ /* attach/detach/access layer */
+      var self = this,
+      layers = self.layers;
+      if(layer === und){
+        return layers[name];
+      }else{
+        if(layer === null){
+          layer = layers[name];
+          if(layer){
+            delete layer.board;
+            delete layer.name;
+            delete layers[name];
+          }
+        }else{
+          layer.board = self;
+          layer.name = name;
+          layers[name] = layer;
+        }
+        self.bound();
+      }
     }
   };
 
---
+++ pcbjs.layout.js
@@ -0,0 +1,85 @@
+define(['nls!.'], function(t){
+  return {
+    /*
+     * Layers configuration
+     *
+     * Gerber file extensions
+     * Kicad/Protel | Eagle | Orcad
+     */
+    /* copper */
+    wire_top: {
+      title: t("Copper Top"),
+      color: 'brown',
+      hatch: 1,
+      index: 10,
+      files: /\.(?:gtl|cmp|top)$/i
+    },
+    wire_bot: {
+      title: t("Copper Bottom"),
+      color: 'green',
+      hatch: 1,
+      index: -10,
+      files: /\.(?:gbl|sol|bot)$/i
+    },
+    /*wire_in$1: {
+      title: t("Copper Inner"),
+      files: /\.g([0-9])$/i
+    },*/
+    /* solder mask */
+    mask_top: {
+      title: t("Mask Top"),
+      color: 'pink',
+      index: 11,
+      files: /\.(?:gts|stc|smt|stoptop|tsm)$/i
+    },
+    mask_bot: {
+      title: t("Mask Bottom"),
+      color: 'blue',
+      index: -11,
+      files: /\.(?:gbs|sts|smb|stopbot|bsm)$/i
+    },
+    /* silk overlay */
+    silk_top: {
+      title: t("Silk Top"),
+      color: 'while',
+      index: 12,
+      files: /\.(?:gto|plc|sst|positop|slk|leg)$/i
+    },
+    silk_bot: {
+      title: t("Silk Bottom"),
+      color: 'cian',
+      index: -12,
+      files: /\.(?:gbo|pls|ssb|posibot|bsk)$/i
+    },
+    /* drill */
+    hole: {
+      title: t("Drill map"),
+      color: 'red',
+      index: 0,
+      files: /\.(?:drl|drd|tap|txt|gdd|cnc|exl|drill)$/i
+    },
+    /* edges */
+    edge: {
+      title: t("Edges/Outline"),
+      color: 'yellow',
+      index: 0,
+      files: /\.(?:gbr|gko|gm[12])$/i
+    }
+    /* stencil */
+    /*
+    sold_top: {
+      title: t("Sold Top"),
+      color: 'black',
+      files: /\.(?:gtp)$/i
+    },
+    sold_bot: {
+      title: t("Sold Bottom"),
+      color: 'gray',
+      files: /\.(?:gbp)$/i
+    }*//*,
+    padm: { // pad master
+      top: /\.(?:gpb)$/i,
+      bot: /\.(?:gpt)$/i
+    }*/
+  };
+});
